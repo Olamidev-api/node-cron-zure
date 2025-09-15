07d808dbce5dd63a4fbed7cc99998172a495f917
Olamide <akinnusivictor098@gmail.com>
2025-09-15 10:00:20 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import cron from 'node-cron';
+
+cron.schedule(`*/1 * * * *`, async () => {
+    fetch("https://zureinsurance-api.onrender.com/swagger-ui/index.html#").then(res=> console.log(res))
+});
